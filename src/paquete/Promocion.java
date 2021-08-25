b611066f3ebff42298de2e3f120e11a6e2fdaecf
Mauro Bustos <bustosrme@gmail.com>
2021-08-25 20:45:14 -0300

    Creación del enum TipoAtracciones

--- src/paquete/Promocion.java
+++ src/paquete/Promocion.java
@@ -2,4 +2,21 @@ package paquete;
 
 public abstract class Promocion implements Adquiribles{
 
+	
+	public void comprar() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public int getCosto() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	public double getTiempo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
---
+++ src/paquete/TipoAtracciones.java
@@ -0,0 +1,12 @@
+package paquete;
+
+public enum TipoAtracciones {
+	
+	ATRACCION_1, ATRACCION_2, ATRACCION_3, ATRACCION_4;
+	
+}
+
+/*Dejo creado el enum con los nombres genericos para ir trabajando con 
+ * ellos hasta armar la temática, luego podemos cambiar todos juntos 
+ * usando el refactor
+ */
