9e3c072935ef2a0861ab3c6736ddbfe9839d9e86
Mauro Bustos <Mauro Bustos@DESKTOP-A8USAHO>
2021-09-03 19:10:55 -0300

    Agregado de la clase Sugerencia con el compare.

---
+++ src/paquete/Sugerencia.java
@@ -0,0 +1,29 @@
+package paquete;
+
+import java.util.Comparator;
+
+public class Sugerencia implements Comparator<Adquirible> {
+	private TipoAtraccion atraccionFavorita;
+
+	public Sugerencia(TipoAtraccion atraccionFavorita) {
+		this.atraccionFavorita = atraccionFavorita;
+
+	}
+
+	public int compare(Adquirible adquirible1, Adquirible adquirible2) {
+		if (adquirible1.getTematica() == atraccionFavorita && adquirible2.getTematica() != atraccionFavorita) {
+			return 1;
+		} else if (adquirible1.getTematica() != atraccionFavorita && adquirible2.getTematica() == atraccionFavorita)
+			return -1;
+		if (adquirible1.esPromocion() && !adquirible2.esPromocion())
+			return 1;
+		else if (!adquirible1.esPromocion() && adquirible2.esPromocion())
+			return -1;
+		if (adquirible1.getCosto() > adquirible2.getCosto())
+			return 1;
+		else if (adquirible1.getCosto() < adquirible2.getCosto())
+			return -1;
+		return (int) (adquirible1.getTiempo() - adquirible2.getTiempo());
+	}
+// Faltaria la validacion si el usuario tiene tiempo, dinero,, si ya se oferto y el cupo 
+}
