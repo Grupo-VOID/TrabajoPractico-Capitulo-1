791cbbc43dd899e68faa74c9c7136eb3ddc24ee5
charly5683 <ojeda.juancarlos02@gmail.com>
2021-09-08 17:31:47 -0300

    Se invierte el metodo compare para cumplir con el orden correcto

--- src/paquete/Sugerencia.java
+++ src/paquete/Sugerencia.java
@@ -7,27 +7,25 @@ public class Sugerencia implements Comparator<Adquirible> {
 
 	public Sugerencia(Usuario usuarioRef) {
 		atraccionFavorita = usuarioRef.getTematica();
-
 	}
 	
 	public TipoAtraccion getAtraccion() {
 		return atraccionFavorita;
 	}
 	
-
 	public int compare(Adquirible adquirible1, Adquirible adquirible2) {
 		if (adquirible1.getTematica() == atraccionFavorita && adquirible2.getTematica() != atraccionFavorita) {
-			return 1;
+			return -1;
 		} else if (adquirible1.getTematica() != atraccionFavorita && adquirible2.getTematica() == atraccionFavorita)
-			return -1;
+			return 1;
 		if (adquirible1.esPromocion() && !adquirible2.esPromocion())
-			return 1;
+			return -1;
 		else if (!adquirible1.esPromocion() && adquirible2.esPromocion())
-			return -1;
-		if (adquirible1.getCosto() > adquirible2.getCosto())
 			return 1;
-		else if (adquirible1.getCosto() < adquirible2.getCosto())
+		if (adquirible1.getCosto() > adquirible2.getCosto())
 			return -1;
+		else if (adquirible1.getCosto() < adquirible2.getCosto())
+			return 1;
 		return (int) (adquirible1.getTiempo() - adquirible2.getTiempo());
 	}
 // Faltaria la validacion si el usuario tiene tiempo, dinero,, si ya se oferto y el cupo 
