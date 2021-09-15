9a71a2f098b75dc61f50dfba034ad8260a03a341
Iván Bonilla <ivanbonilla25@gmail.com>
2021-09-15 01:16:36 -0300

    Se termina la Clase PromocionesTest

--- test/paquete/PromocionesTest.java
+++ test/paquete/PromocionesTest.java
@@ -7,42 +7,116 @@ import org.junit.Test;
 
 public class PromocionesTest {
 
-	ParqueAtracciones parque;
+	Atraccion atraccion1;
+	Atraccion atraccion2;
+	Atraccion atraccionGratis;
 
 	@Before
 	public void setup() {
-		parque = new ParqueAtracciones();
-		parque.agregarUsuarios(LectorArchivos.GenerarUsuarios());
-		parque.agregarAtracciones(LectorArchivos.GenerarAtracciones());
-		parque.agregarPromociones(LectorArchivos.GenerarPromociones(parque));
+		atraccion1 = new Atraccion("unaAtraccion", TipoAtraccion.COMEDIA, 20.5, 2, 50);
+		atraccion2 = new Atraccion("otraAtraccion", TipoAtraccion.COMEDIA, 12, 5, 40);
+		atraccionGratis = new Atraccion("siguienteAtraccion", TipoAtraccion.COMEDIA, 7.5, 6, 30);
 	}
 
 	@Test
-	public void costoDeLasPromociones() {
+	public void costoYTiempoPromocionAbsoluta() {
 
-		// Procentual
+		// descuento 15 monedas
+		PromocionAbsoluta promocionAbsoluta = new PromocionAbsoluta(TipoAtraccion.COMEDIA, atraccion1, atraccion2, 15);
 
-		double costoPromoPorcentual = parque.getPromociones().get(0).getCosto();
-		double costoPruebaPorcentual = parque.getAtracciones().get(9).getCosto()
-				+ parque.getAtracciones().get(10).getCosto();
+		assertEquals(promocionAbsoluta.getCosto(), atraccion1.getCosto() + atraccion2.getCosto() - 15, 0);
+		assertEquals(promocionAbsoluta.getTiempo(), atraccion1.getTiempo() + atraccion2.getTiempo(), 0);
+
+	}
+
+	@Test
+	public void DisponibilidadYCompraPromocionAbsoluta() {
+
+		// descuento 15 monedas
+		PromocionAbsoluta promocionAbsoluta = new PromocionAbsoluta(TipoAtraccion.COMEDIA, atraccion1, atraccion2, 15);
+
+		assertTrue(promocionAbsoluta.estaDisponible());
+
+		int i = 1;
+		while (i <= 40) {
+			promocionAbsoluta.comprar();
+			i++;
+		}
+
+		assertFalse(promocionAbsoluta.estaDisponible());
+
+		assertEquals(atraccion1.getCupoActual(), 40);
+		assertEquals(atraccion2.getCupoActual(), 40);
+
+	}
+
+	@Test
+	public void costoYTiempoPromocionPorcentual() {
+
+		// descuento 20%
+		PromocionPorcentual promocionPorcentual = new PromocionPorcentual(TipoAtraccion.COMEDIA, atraccion1, atraccion2,
+				20);
+
+		double costoPruebaPorcentual = atraccion1.getCosto() + atraccion2.getCosto();
 		costoPruebaPorcentual = costoPruebaPorcentual * (1.00 - 20 / 100.00);
 
-		assertEquals(costoPromoPorcentual, costoPruebaPorcentual, 0);
+		assertEquals(promocionPorcentual.getCosto(), costoPruebaPorcentual, 0);
+		assertEquals(promocionPorcentual.getTiempo(), atraccion1.getTiempo() + atraccion2.getTiempo(), 0);
 
-		// AxB
+	}
 
-		double costoPromoAxB = parque.getPromociones().get(2).getCosto();
-		double costoPruebaAxB = parque.getAtracciones().get(8).getCosto() + parque.getAtracciones().get(3).getCosto();
+	@Test
+	public void DisponibilidadYCompraPromocionPorcentual() {
 
-		assertEquals(costoPromoAxB, costoPruebaAxB, 0);
+		PromocionPorcentual promocionPorcentual = new PromocionPorcentual(TipoAtraccion.COMEDIA, atraccion1, atraccion2,
+				15);
 
-		// Absoluta
+		assertTrue(promocionPorcentual.estaDisponible());
 
-		double costoPromoAbsoluta = parque.getPromociones().get(1).getCosto();
-		double costoPruebaAbsoluta = parque.getAtracciones().get(6).getCosto()
-				+ parque.getAtracciones().get(4).getCosto() - 10;
+		int i = 1;
+		while (i <= 40) {
+			promocionPorcentual.comprar();
+			i++;
+		}
 
-		assertEquals(costoPromoAbsoluta, costoPruebaAbsoluta, 0);
+		assertFalse(promocionPorcentual.estaDisponible());
+
+		assertEquals(atraccion1.getCupoActual(), 40);
+		assertEquals(atraccion2.getCupoActual(), 40);
+
+	}
+
+	@Test
+	public void costoYTiempoPromocionAxB() {
+
+		// descuento 15 monedas
+		PromocionAxB promocionAxB = new PromocionAxB(TipoAtraccion.COMEDIA, atraccion1, atraccion2, atraccionGratis);
+
+		assertEquals(promocionAxB.getCosto(), atraccion1.getCosto() + atraccion2.getCosto(), 0);
+		assertEquals(promocionAxB.getTiempo(),
+				atraccion1.getTiempo() + atraccion2.getTiempo() + atraccionGratis.getTiempo(), 0);
+
+	}
+
+	@Test
+	public void DisponibilidadYCompraPromocionAxB() {
+
+		PromocionAxB promocionAxB = new PromocionAxB(TipoAtraccion.COMEDIA, atraccion1, atraccion2, atraccionGratis);
+
+		assertTrue(promocionAxB.estaDisponible());
+
+		int i = 1;
+		while (i <= 30) {
+			promocionAxB.comprar();
+			i++;
+		}
+
+		assertFalse(promocionAxB.estaDisponible());
+
+		assertEquals(atraccion1.getCupoActual(), 30);
+		assertEquals(atraccion2.getCupoActual(), 30);
+		assertEquals(atraccionGratis.getCupoActual(), 30);
+
 	}
 
 }
